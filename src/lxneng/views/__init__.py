c1ed584fa6179d23174a80f16522267361640240
Eric Lo <lxneng@gmail.com>
2011-09-08 23:08:28 +0800

    serving robots.txt from root

--- src/lxneng/views/__init__.py
+++ src/lxneng/views/__init__.py
@@ -1,4 +1,17 @@
+import os
+
 from pyramid.view import view_config
+from pyramid.response import Response
+
+
+_here = os.path.dirname(__file__)
+_robots = open(os.path.join(_here, '../static', 'robots.txt')).read()
+_robots_response = Response(content_type='text/plain', body=_robots)
+
+
+@view_config(name='robots.txt')
+def robotstxt_view(context, request):
+    return _robots_response
 
 
 @view_config(context='pyramid.exceptions.NotFound', renderer='404.html')
