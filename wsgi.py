e5a3629217f6439574d17424b8ad814649124505
Eric Lo <lxneng@gmail.com>
2011-09-13 18:22:28 +0800

    update wsgi config

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
-import os
+import sys
 from paste.deploy import loadapp
 from paste.httpserver import serve
 
-here = os.path.abspath(os.path.dirname(__file__))
-config_file_path = os.path.join(here, 'development.ini')
-application = loadapp('config:%s' % config_file_path)
+
+if __name__ == '__main__':
+    serve(loadapp('config:/Users/eric/Projects/lxneng/development.ini'), host='0.0.0.0', port=8088) 
+else:
+    sys.path.append('/home/dotcloud/code')
+    application = loadapp('config:/home/dotcloud/code/development.ini')
+
