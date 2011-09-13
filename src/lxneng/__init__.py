327c69f17a1b69bb5b00864fa3400cc383e5af81
Eric Lo <lxneng@gmail.com>
2011-09-13 18:29:56 +0800

    bug fix

--- src/lxneng/__init__.py
+++ src/lxneng/__init__.py
@@ -8,7 +8,7 @@ def main(global_config, **settings):
     settings.setdefault('jinja2.i18n.domain', 'lxneng')
     config = Configurator(settings=settings)
     includeme(config)
-    config.add_translation_dirs('locale/')
+    config.add_translation_dirs('lxneng:locale')
     config.add_renderer('.html', renderer_factory)
     config.add_static_view('static', 'lxneng:static', cache_max_age=3600)
     config.add_route("home", "/")
