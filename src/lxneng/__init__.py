99a16346bce5811a1b814354d4094c384301d827
Eric Lo <lxneng@gmail.com>
2011-09-28 22:20:20 +0800

    auth config

--- src/lxneng/__init__.py
+++ src/lxneng/__init__.py
@@ -2,19 +2,26 @@ from pyramid.config import Configurator
 from pyramid_jinja2 import renderer_factory
 from s4u.sqlalchemy import includeme
 from lxneng import factories
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid_beaker import session_factory_from_settings
 from pyramid_beaker import set_cache_regions_from_settings
 
 def main(global_config, **settings):
     set_cache_regions_from_settings(settings)
-    config = Configurator(settings=settings)
+    config = Configurator(settings=settings,
+            session_factory=session_factory_from_settings(settings),
+            root_factory=factories.RootFactory,
+            authentication_policy=AuthTktAuthenticationPolicy('secret'),
+            authorization_policy=ACLAuthorizationPolicy()
+    )
     includeme(config)
-    session_factory = session_factory_from_settings(settings)
-    config.set_session_factory(session_factory)
     config.add_translation_dirs('lxneng:locale')
     config.add_renderer('.html', renderer_factory)
     config.add_static_view('static', 'lxneng:static', cache_max_age=3600)
     config.add_route("home", "/")
+    config.add_route("login", "/login")
+    config.add_route("logout", "/logout")
     config.add_route("about", "/about")
     config.add_route("blog", "/blog")
     config.add_route("photos", "/photos")
