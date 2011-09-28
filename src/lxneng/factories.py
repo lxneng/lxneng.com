6808eb8ab15a579df3e30967381e7b349b5dccb7
Eric Lo <lxneng@gmail.com>
2011-09-28 22:21:07 +0800

    add RootFactory

--- src/lxneng/factories.py
+++ src/lxneng/factories.py
@@ -1,6 +1,9 @@
 from pyramid.exceptions import NotFound
 from s4u.sqlalchemy import meta
 from lxneng.models import Post
+from pyramid.security import Everyone
+from pyramid.security import Authenticated
+from pyramid.security import Allow
 
 
 def SimpleTypeFactory(cls):
@@ -20,3 +23,13 @@ def SimpleTypeFactory(cls):
 
 
 PostFactory = SimpleTypeFactory(Post)
+
+
+class RootFactory(object):
+    __acl__ = [
+            (Allow, Everyone, 'view'),
+            (Allow, Authenticated, 'auth')
+    ]
+
+    def __init__(self, request):
+        pass
