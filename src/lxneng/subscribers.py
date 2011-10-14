d8928f4c0ec57aba4f2d68441ac4682740a22c97
Eric Lo <lxneng@gmail.com>
2011-10-14 13:53:17 +0800

    add new event

--- src/lxneng/subscribers.py
+++ src/lxneng/subscribers.py
@@ -1,7 +1,22 @@
 from pyramid.events import subscriber
 from pyramid.events import BeforeRender
+from pyramid.events import NewRequest
+from flatland.out.markup import Generator
+from pyramid.httpexceptions import HTTPForbidden
 from lxneng.utils import Tools
+from lxneng.utils import get_user
+
 
 @subscriber(BeforeRender)
 def add_renderer_globals(event):
     event['tools'] = Tools(event['request'])
+    event['html'] = Generator()
+    event['user'] = get_user(event['request'])
+
+
+@subscriber(NewRequest)
+def csrf_validation(event):
+    if event.request.method == "POST":
+        token = event.request.POST.get("_csrf")
+        if token is None or token != event.request.session.get_csrf_token():
+            raise HTTPForbidden('CSRF token is missing or invalid')
